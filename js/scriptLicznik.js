3d8fb3cc961007c2ce281f5e9da008619feff25a
Kamil Smoczynski <kamilsmoczynski9309@gmail.com>
2023-02-13 21:07:26 +0100

    Optimize code and add protection against re-clicking a running counter

--- js/scriptLicznik.js
+++ js/scriptLicznik.js
@@ -8,147 +8,129 @@ const timeClock = () => {
 	clock.innerHTML = d.toLocaleTimeString()
 }
 
-timeClock()
+// timeClock()
 
 const currentData = () => {
 	let date = new Date().toLocaleDateString()
 	currentDay.innerHTML = date
 }
 
-currentData()
+// currentData()
 
 // Stoper function
+const workSquare = document.querySelector('.counter__work')
+const measurementsSquare = document.querySelector('.counter__measurements')
+const rearmingSquare = document.querySelector('.counter__rearming')
+const failureSquare = document.querySelector('.counter__failure')
 
+let workCounter = 0
+let workInterval = null
 
-let workCounter = 0;
-let workInterval = null;
-let isWorkStopped = false;
+let measurementsCounter = 0
+let measurementsInterval = null
 
-let measurementsCounter = 0;
-let measurementsInterval = null;
-let isMeasurementsStopped = false;
+let rearmingCounter = 0
+let rearmingInterval = null
 
-let rearmingCounter = 0;
-let rearmingInterval = null;
-let isRearmingStopped = false;
-
-let failureCounter = 0;
-let failureInterval = null;
-let isFailureStopped = false;
+let failureCounter = 0
+let failureInterval = null
 
 function displayTime(counter, element) {
-    let minutes = Math.floor(counter / 60);
-    let seconds = counter % 60;
-    let hours = Math.floor(minutes / 60);
-    minutes %= 60;
-    element.innerText =
-      (hours < 10 ? "0" + hours : hours) +
-      ":" +
-      (minutes < 10 ? "0" + minutes : minutes) +
-      ":" +
-      (seconds < 10 ? "0" + seconds : seconds);
-  }
-
-const workSquare = document.querySelector(".counter__work");
-if (workSquare) {
-  workSquare.addEventListener("click", () => {
-    clearInterval(measurementsInterval);
-    isMeasurementsStopped = true;
-  
-    clearInterval(rearmingInterval);
-    isRearmingStopped = true;
-  
-    clearInterval(failureInterval);
-    isFailureStopped = true;
-  
-    if (isWorkStopped) {
-      isWorkStopped = false;
-    } else {
-      clearInterval(workInterval);
-      workCounter = 0;
-      isWorkStopped = true;
-    }
-  
-    workInterval = setInterval(() => {
-      workCounter++;
-      displayTime(workCounter, document.querySelector(".work-time"));
-    }, 1000);
-  });
+	let minutes = Math.floor(counter / 60)
+	let seconds = counter % 60
+	let hours = Math.floor(minutes / 60)
+	minutes %= 60
+	element.innerText =
+		(hours < 10 ? '0' + hours : hours) +
+		':' +
+		(minutes < 10 ? '0' + minutes : minutes) +
+		':' +
+		(seconds < 10 ? '0' + seconds : seconds)
 }
 
-const measurementsSquare = document.querySelector(".counter__measurements");
-if (measurementsSquare) {
-  measurementsSquare.addEventListener("click", () => {
-    clearInterval(workInterval);
-    isWorkStopped = true;
-  
-    clearInterval(rearmingInterval);
-    isRearmingStopped = true;
-  
-    clearInterval(failureInterval);
-    isFailureStopped = true;
-  
-    if (isMeasurementsStopped) {
-      isMeasurementsStopped = false;
-    } else {
-      clearInterval(measurementsInterval);
-      measurementsCounter = 0;
-      isMeasurementsStopped = true;
-    }
-  
-    measurementsInterval = setInterval(() => {
-      measurementsCounter++;
-      displayTime(
-        measurementsCounter,
-        document.querySelector(".measurements-time")
-      );
-    }, 1000);
-  });
+function startWorkInterval() {
+	workInterval = setInterval(() => {
+		workCounter++
+		displayTime(workCounter, document.querySelector('.work-time'))
+	}, 1000)
+	workSquare.removeEventListener('click', work)
+}
+function startMeasurementsInterval() {
+	measurementsInterval = setInterval(() => {
+		measurementsCounter++
+		displayTime(measurementsCounter, document.querySelector('.measurements-time'))
+	}, 1000)
+	measurementsSquare.removeEventListener('click', measurements)
 }
 
-const rearmingSquare = document.querySelector(".counter__rearming");
-rearmingSquare.addEventListener("click", () => {
-clearInterval(workInterval);
-isWorkStopped = true;
-
-clearInterval(measurementsInterval);
-isMeasurementsStopped = true;
-
-clearInterval(failureInterval);
-isFailureStopped = true;
-
-if (isRearmingStopped) {
-rearmingCounter = rearmingCounter;
-isRearmingStopped = false;
-} else {
-clearInterval(rearmingInterval);
-rearmingCounter = 0;
-isRearmingStopped = true;
+function startRearmingInterval() {
+	rearmingInterval = setInterval(() => {
+		rearmingCounter++
+		displayTime(rearmingCounter, document.querySelector('.rearming-time'))
+	}, 1000)
+	rearmingSquare.removeEventListener('click', rearming)
+}
+function startFailureInterval() {
+	failureInterval = setInterval(() => {
+		failureCounter++
+		displayTime(failureCounter, document.querySelector('.failure-time'))
+	}, 1000)
+	failureSquare.removeEventListener('click', failure)
 }
 
-rearmingInterval = setInterval(() => {
-rearmingCounter++;
-displayTime(rearmingCounter, document.querySelector(".rearming-time"));
-}, 1000);
-});
+function work() {
+	clearInterval(measurementsInterval)
 
-const failureSquare = document.querySelector(".counter__failure");
-failureSquare.addEventListener("click", () => {
-clearInterval(workInterval);
-clearInterval(measurementsInterval);
-clearInterval(rearmingInterval);
+	clearInterval(rearmingInterval)
 
-if (isFailureStopped) {
-failureCounter = failureCounter;
-isFailureStopped = false;
-} else {
-clearInterval(failureInterval);
-failureCounter = 0;
-isFailureStopped = true;
+	clearInterval(failureInterval)
+
+	startWorkInterval()
 }
 
-failureInterval = setInterval(() => {
-failureCounter++;
-displayTime(failureCounter, document.querySelector(".failure-time"));
-}, 1000);
-});
+function measurements() {
+	clearInterval(workInterval)
+
+	clearInterval(rearmingInterval)
+
+	clearInterval(failureInterval)
+
+	startMeasurementsInterval()
+
+	workSquare.addEventListener('click', work)
+	rearmingSquare.addEventListener('click', rearming)
+	failureSquare.addEventListener('click', failure)
+}
+
+function rearming() {
+	clearInterval(workInterval)
+
+	clearInterval(measurementsInterval)
+
+	clearInterval(failureInterval)
+
+	startRearmingInterval()
+
+	workSquare.addEventListener('click', work)
+	measurementsSquare.addEventListener('click', measurements)
+	failureSquare.addEventListener('click', failure)
+}
+
+function failure() {
+	clearInterval(workInterval)
+
+	clearInterval(measurementsInterval)
+
+	clearInterval(rearmingInterval)
+
+	startFailureInterval()
+
+	workSquare.addEventListener('click', work)
+	measurementsSquare.addEventListener('click', measurements)
+	rearmingSquare.addEventListener('click', rearming)
+}
+
+workSquare.addEventListener('click', work)
+measurementsSquare.addEventListener('click', measurements)
+rearmingSquare.addEventListener('click', rearming)
+failureSquare.addEventListener('click', failure)
